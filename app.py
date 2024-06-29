e92c9cc080632e5787964de84b7ed341132d8c09
msivakumars <msivakumars@hotmail.com>
2024-06-29 15:25:54 +0530

    db(migrate): db migration enabled

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from db import db
 from resources.employee import blp as employee_blue_print
 import os
 
+
 def create_app():
     app = Flask(__name__)
     load_dotenv()
@@ -18,7 +19,8 @@ def create_app():
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.fsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
+    db.init_app(app)
+    migrate = Migrate(app, db)
     api = Api(app)
     api.register_blueprint(blp=employee_blue_print)
     return app
